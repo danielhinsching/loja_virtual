1828fa874c243c549794aba9b25de782327a8ddd
danielhinsching <danielsantoshinsching@gmail.com>
2024-04-03 16:30:17 -0300

    Alterações Layouts

---
+++ src/layouts/LayoutSmall.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SmallHeader from '../components/templates/Small/SmallHeader.vue';
+import SmallFooter  from '../components/templates/Small/SmallFooter.vue';
+</script>
+<template>
+    <header>
+        <SmallHeader/>        
+    </header>
+    <main>
+        <RouterView/>
+    </main>
+    <footer>
+        <SmallFooter/>
+    </footer>
+</template>
