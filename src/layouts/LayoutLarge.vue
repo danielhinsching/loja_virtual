0212b3217572193ee912a39d0c0d2363b1b04a53
danielhinsching <danielsantoshinsching@gmail.com>
2024-04-03 16:25:41 -0300

    alterações layouts Large e Medium

---
+++ src/layouts/LayoutLarge.vue
@@ -0,0 +1,19 @@
+<script setup>
+import LargeAside from '../components/templates/large/LargeAside.vue';
+import LargeHeader from '../components/templates/large/LargeHeader.vue';
+import LargeFooter  from '../components/templates/large/LargeFooter.vue';
+</script>
+<template>
+    <header>
+        <LargeHeader/>        
+    </header>
+    <aside>
+        <LargeAside/>
+    </aside>
+    <main>
+        <RouterView/>
+    </main>
+    <footer>
+        <LargeFooter/>
+    </footer>
+</template>
---
+++ src/layouts/LayoutMedium.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MediumAside from '../components/templates/Medium/MediumAside.vue';
+import MediumHeader from '../components/templates/Medium/MediumHeader.vue';
+import MediumFooter  from '../components/templates/Medium/MediumFooter.vue';
+</script>
+<template>
+    <header>
+        <MediumHeader/>        
+    </header>
+    <aside>
+        <MediumAside/>
+    </aside>
+    <main>
+        <RouterView/>
+    </main>
+    <footer>
+        <MediumFooter/>
+    </footer>
+</template>
